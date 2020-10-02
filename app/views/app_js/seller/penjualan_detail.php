93e7b7e357089ee68a84a7b8093024cb545fd630
wahyucyber <m.nur.wahyu1974@gmail.com>
2020-10-03 02:16:08 +0700

    mengembalikan ke semula status pembelian

--- app/views/app_js/seller/penjualan_detail.php
+++ app/views/app_js/seller/penjualan_detail.php
@@ -269,7 +269,26 @@
 														<div class="step--status-value"></div>
 													</div>
 													<div class="step--status-list">
-														${status}
+														<div class="step--list-item" data-status="Menunggu diproses" data-toggle="tooltip" data-placement="top" title="Tooltip on top">
+															<div class="list-item-content" style="">
+															</div>
+															<small><i class="text-orange fal fa-smile"></i> Menunggu diproses</small>
+														</div>
+														<div class="step--list-item" data-status="Diproses" data-toggle="tooltip" data-placement="top" title="Tooltip on top">
+															<div class="list-item-content" style="">
+															</div>
+															<small><i class="text-orange fal fa-box"></i> diproses</small>
+														</div>
+														<div class="step--list-item" data-status="Dikirim" data-toggle="tooltip" data-placement="top" title="Tooltip on top">
+															<div class="list-item-content" style="">
+															</div>
+															<small><i class="text-orange fal fa-car-side"></i> Dikirim</small>
+														</div>
+														<div class="step--list-item" data-status="Selesai" data-toggle="tooltip" data-placement="top" title="Tooltip on top">
+															<div class="list-item-content" style="">
+															</div>
+															<small><i class="text-orange fal fa-box-open"></i> Diterima</small>
+														</div>
 													</div>
 												</div>
 											</div>
