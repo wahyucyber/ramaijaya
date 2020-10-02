fe497b2ba897a0078371c703a4b4f54774660c3c
wahyucyber <m.nur.wahyu1974@gmail.com>
2020-10-03 02:27:18 +0700

    menambahkan title tanggal pada status pembelian

--- app/views/app_js/seller/penjualan_detail.php
+++ app/views/app_js/seller/penjualan_detail.php
@@ -162,22 +162,22 @@
 														<div class="step--status-value"></div>
 													</div>
 													<div class="step--status-list">
-														<div class="step--list-item" data-status="Menunggu diproses" data-toggle="tooltip" data-placement="top" title="Tooltip on top">
+														<div class="step--list-item" data-status="Menunggu diproses" data-toggle="tooltip" data-placement="top" title="${res.Data.created_at}">
 															<div class="list-item-content" style="">
 															</div>
 															<small><i class="text-orange fal fa-smile"></i> Menunggu diproses</small>
 														</div>
-														<div class="step--list-item" data-status="Diproses" data-toggle="tooltip" data-placement="top" title="Tooltip on top">
+														<div class="step--list-item" data-status="Diproses" data-toggle="tooltip" data-placement="top" title="${res.Data.log_diproses}">
 															<div class="list-item-content" style="">
 															</div>
 															<small><i class="text-orange fal fa-box"></i> diproses</small>
 														</div>
-														<div class="step--list-item" data-status="Dikirim" data-toggle="tooltip" data-placement="top" title="Tooltip on top">
+														<div class="step--list-item" data-status="Dikirim" data-toggle="tooltip" data-placement="top" title="${res.Data.dikirim}">
 															<div class="list-item-content" style="">
 															</div>
 															<small><i class="text-orange fal fa-car-side"></i> Dikirim</small>
 														</div>
-														<div class="step--list-item" data-status="Selesai" data-toggle="tooltip" data-placement="top" title="Tooltip on top">
+														<div class="step--list-item" data-status="Selesai" data-toggle="tooltip" data-placement="top" title="${res.Data.diterima}">
 															<div class="list-item-content" style="">
 															</div>
 															<small><i class="text-orange fal fa-box-open"></i> Diterima</small>
