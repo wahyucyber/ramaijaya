ba1308a8518a18cd86f2587c80a842f901cf0aad
wahyucyber <m.nur.wahyu1974@gmail.com>
2020-10-03 02:03:13 +0700

    menambahkan content kurir pada tab 8

--- app/views/seller/setting/shop/tab_8.php
+++ app/views/seller/setting/shop/tab_8.php
@@ -1,13 +1,22 @@
 <div class="card-body">
-	<div class="row">
-		<div class="col-md-7">
-			<h5>Daftar Template Balasan</h5>
-			<small>Anda dapat membuat 5 template balasan ulasan</small>
-		</div>
-		<div class="col-md-5 text-right">
-			<a href="" class="btn btn-primary">Tambah Template</a>
-		</div>
-	</div>
+   <div class="row">
+            
+      <div class="col-md-6 mb-3">
+         <h4 class="fs-18 badge badge-success bg-orange"><i class="fal fa-car-side "></i> Kurir</h4>
+      </div>
+
+      <div class="col-md-6 mb-3" align="right">
+         <button class="btn btn-success btn-sm" data-toggle="modal" data-target="#add"><i class="fa fa-plus"></i> Tambah Kurir</button>
+         <button class="btn btn-warning btn-sm btn-refresh"><i class="fal fa-retweet"></i> Refresh</button>
+      </div>
+
+      <hr>
+
+      <div class="col-md-12 list--kurir">
+         <img src="<?php echo base_url('assets/img/default/loader.gif'); ?>" style="width: 10%;" alt="">
+      </div>
+
+   </div>
 	<ul class="list-group mt-3">
 	  <li class="list-group-item bg-light">
 	  	<div class="clearfix">
@@ -32,4 +41,49 @@
 	  </li>
 	
 	</ul>
+</div>
+
+<div class="modal fade" id="add">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header bg-orange text-white">
+				<h5>Tambah Kurir</h5>
+			</div>
+			<form class="add">
+				<div class="modal-body">
+					<select name="" id="" class="form-control add--kurir select2">
+						<option value="">-Pilih Kurir-</option>
+						<option value="jnt">J&T</option>
+						<option value="jne">JNE</option>
+						<option value="tiki">TIKI</option>
+						<option value="pos">POS</option>
+					</select>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-danger btn-sm" data-dismiss="modal"><i class="fal fa-times-circle"></i>	Batal</button>
+					<button type="submit" class="btn btn-success btn-sm"><i class="fal fa-save"></i>	Simpan</button>
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
+
+<div class="modal fade" id="delete">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header bg-danger text-white">
+				<h5>Konfirmasi</h5>
+			</div>
+			<div class="modal-body">
+				<p class="alert alert-warning">
+					Apakah anda yaking ingin menghapus kurir <b class="nama--kurir">Nama Kurir ?</b>
+				</p>
+			</div>
+			<div class="modal-footer">
+				<input type="hidden" name="" class="code">
+				<button type="button" class="btn btn-warning btn-sm" data-dismiss="modal"><i class="fal fa-times-circle"></i>	Batal</button>
+				<button type="button" class="btn btn-danger btn-sm ya--hapus"><i class="fal fa-check-circle"></i>	Ya</button>
+			</div>
+		</div>
+	</div>
 </div>
