a05658ee4d024527022d27c906fca13bf231fd23
wahyucyber <m.nur.wahyu1974@gmail.com>
2020-10-03 02:01:08 +0700

    menambahkan tab 8 dan kurir php

---
+++ app/views/app_js/seller/setting/shop/kurir.php
@@ -0,0 +1,126 @@
+<?php include __DIR__.'/../../Seller.php' ?>
+
+<script>
+	class Kurir {
+		constructor() {
+			this.run();
+		}
+
+		run() {
+			let output = $("div.list--kurir").html('');
+			callApi("seller/kurir", {
+				client_token: $jp_client_token
+			}, res => {
+				if (res.Data.length == 0) {
+					notif("div.list--kurir", "warning", "Data tidak ada.");
+				}else{
+					let kurir = `<div class="row">`;
+					$.each(res.Data, function(index, val) {
+						let kurir_img = "";
+						let kurir_nama = "";
+						if (val.code == "jnt") {
+							kurir_img = `<img src="<?php echo base_url('assets/img/kurir/jnt.png'); ?>" alt="">`;
+							kurir_nama = " J&T Express";
+						}else if (val.code == "jne") {
+							kurir_img = `<img src="<?php echo base_url('assets/img/kurir/jne.png'); ?>" alt="">`;
+							kurir_nama = "JNE Express";
+						}else if (val.code == "tiki") {
+							kurir_img = `<img src="<?php echo base_url('assets/img/kurir/tiki.png'); ?>" alt="">`;
+							kurir_nama = "TIKI";
+						}else if (val.code == "pos") {
+							kurir_img = `<img src="<?php echo base_url('assets/img/kurir/pos.jpg'); ?>" alt="">`;
+							kurir_nama = "POS Indonesia";
+						}
+
+						kurir += `
+							<div class="col-md-3">
+				      			<div class="kurir shadow">
+				      				<div class="kurir-body">
+				      				${kurir_img}
+				      				</div>
+				      				<div class="kurir-footer fs-12">
+				      					${kurir_nama}
+				      				</div>
+				      				<div class="kurir-action">
+				      					<button class="btn btn-danger btn-sm kurir--delete" data-toggle="modal" data-target="#delete" data-code="${val.code}"><i class="fal fa-trash-alt"></i></button>
+				      				</div>
+				      			</div>
+				      		</div>
+						`;
+					});
+					kurir += `</div>`;
+					output.append(kurir);
+				}
+			})
+		}
+
+		add(params) {
+			let code = params['code'];
+
+			callApi("seller/kurir/add", {
+				client_token: $jp_client_token,
+				code: code
+			}, res => {
+				let message = res.Message;
+				if (res.Error == true) {
+					notifAlert(message, "error", 5);
+				}else{
+					$("div#add").modal('hide');
+					$("select.add--kurir").val('').trigger('change');
+					notifAlert(message, "success", 5);
+					kurir.run();
+				}
+			})
+		}
+
+		delete(params) {
+			let code = params['code'];
+
+			callApi("seller/kurir/delete", {
+				client_token: $jp_client_token,
+				code: code
+			}, res => {
+				let message = res.Message;
+				if (res.Error == true) {
+					notifAlert(message, "error", 5);
+				}else{
+					$("div#delete").modal('hide');
+					$("input.code").val('');
+					notifAlert(message, "success", 5);
+					kurir.run();
+				}
+			})
+		}
+	}
+
+	let kurir = new Kurir;
+
+	$(document).on("submit", "form.add", function () {
+		let code = $("select.add--kurir").val();
+
+		kurir.add({
+			code: code
+		})
+
+		return false;
+	})
+
+	$(document).on("click", "button.btn-refresh", function () {
+		kurir.run();
+	})
+
+	$(document).on("click", "button.kurir--delete", function () {
+		let code = $(this).data('code');
+		$("input.code").val(code);
+	})
+
+	$(document).on("click", "button.ya--hapus", function () {
+		let code = $("input.code").val();
+
+		kurir.delete({
+			code: code
+		})
+
+		return false;
+	})
+</script>
---
+++ app/views/seller/setting/shop/tab_8.php
@@ -0,0 +1,35 @@
+<div class="card-body">
+	<div class="row">
+		<div class="col-md-7">
+			<h5>Daftar Template Balasan</h5>
+			<small>Anda dapat membuat 5 template balasan ulasan</small>
+		</div>
+		<div class="col-md-5 text-right">
+			<a href="" class="btn btn-primary">Tambah Template</a>
+		</div>
+	</div>
+	<ul class="list-group mt-3">
+	  <li class="list-group-item bg-light">
+	  	<div class="clearfix">
+	  		<div class="float-left">
+	  			<h6>
+	  				<b>Balasan Ulasan Otomatis</b>
+		  			
+	  			</h6>
+	  		</div>
+	  		<div class="float-right">
+	  			<a href="" class="card-link text-primary">Ubah Template</a>
+	  		</div>
+	  	</div>
+	  </li>
+
+	  <li class="list-group-item">
+	  	<h6>
+	  		<b>Template Balasan Otomatis</b>
+	  		<span class="badge badge-warning">Balasan Otomatis</span>
+	  	</h6>
+	  	<small>Terima kasih telah berbelanja di Desa IT. Bagikan link toko kami https://www.tokopedia.com/desait kepada teman-teman Anda dan favoritkan Toko kami untuk terus update mengenai stok dan produk terbaru</small>
+	  </li>
+	
+	</ul>
+</div>
